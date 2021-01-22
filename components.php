6bc251c6dd0e54a0e5a183b6d2183daa386d9f8f
ProgramistaZpolski <piotrekbadelek@gmail.com>
2021-01-22 13:52:25 +0100

    super header 10/10

---
+++ components.php
@@ -0,0 +1,8 @@
+<?php
+
+class Components
+{
+	public function listing(string $title = "Hotel", string $price = "50", array $ratings = ["5", "5"])
+	{
+	}
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include_once "components.php";
+$cmp = new Components();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kasztan Travel</title>
+	<meta name="description" content="Kasztan Travel, bardzo realna strona z podrózami - by Kasztan Space">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Kasztan Travel">
+	<meta name="author" content="Kasztan Space">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<!--[if IE]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+	<header class="flexbox bg-danger">
+		<div class="column m7">
+			<h1 class="d2">Kasztan Travel</h1>
+			<h2><i>Podróżuj z kasztanami!</i></h2>
+			<button class="btn btn-success btn-lg">Zobacz teraz!</button>
+			<button class="btn btn-normal m2" disabled>Zaloguj się i buduj swój paszport! (Wkrótce!)</button>
+		</div>
+		<div class="column">
+			<img src="./hero.svg" decoding="async" loading="lazy" alt="Travel" class="img-fluid">
+		</div>
+	</header>
+	<main>
+		
+	</main>
+	<footer></footer>
+</body>
+
+</html>
